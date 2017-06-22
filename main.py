788462b11bdc240ae3089fbc38995812e86a4619
Haile Asnake <hasnake@sdg-komo-mac014.local>
2017-06-21 18:12:32 -0700

    adding username

--- main.py
+++ main.py
@@ -1,42 +1,61 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import cgi
+import re
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+def is_invalid(text):
+    # text == '' or (' ' in text) or len(text) < 3 or len(text) > 20
+    return re.search(r"\s+", text) or re.search(r"^.{0,2}$", text) or re.search(r"^.{20,}$", text)
+
+
+@app.route("/", methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    c_password = request.form['c_password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    c_password_error = ''
+    email_error = ''
+
+    if is_invalid(username):
+        username_error = "That's not a valid username"
+    if is_invalid(password):
+        password_error = "That's not a valid password"
+    if c_password == '':
+        c_password_error = "That's not a valid password"
+    if c_password != password:
+        c_password_error = "Passwords don't match"
+    if email != '' and (email.count("@") != 1 or email.count(".") != 1 or is_invalid(email)):
+        email_error = "That's not a valid email"
+
+
+    if not (username_error or password_error or c_password_error or email_error):
+        return redirect('/welcome?username=' + username)
+    else:
+        return render_template('forms.html',
+            username = username,
+            email = email,
+            username_error = username_error,
+            password_error = password_error,
+            c_password_error = c_password_error,
+            email_error = email_error)
+
+
+@app.route("/welcome")
+def welcome():
+    username = request.args.get('username')
+    return render_template('welcome.html', username=username)
+
 
 @app.route("/")
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eeeeee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-      <!-- create your form here -->
-    <form method="post">
-        <label>Rotate by:
-            <input type="text" name="rot" value="0"/>
-        </label>
-        <br><br>
-            <textarea name="text">{0}</textarea>
-            <input type="submit" value="Submit Query"/>
-    </body>
-</html>
-"""
 def index():
-    return "Hello World"
+    return render_template('forms.html')
+
 
 app.run()
